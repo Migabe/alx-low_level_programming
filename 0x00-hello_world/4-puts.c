637d9b170f7d6405b3e03ba5a13845ebe52a6f96
migabe <migabev@gmail.com>
2022-09-08 19:02:16 +0000

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * print_job - prints words using put
+ * main - prints words using put
  *
  * Return: always 0 (success)
  */
- 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
